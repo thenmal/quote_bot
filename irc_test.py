6a6d7a1f304239e23c64b8965b81b870c0134a81
Quasar Jarosz <quasar@ja.rosz.org>
2013-03-18 15:26:35 -0500

    various changes

--- irc_test.py
+++ irc_test.py
@@ -31,17 +31,20 @@ from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
 import time
 
 class Quote(object):
-    def __init__(self, search, quote):
+    def __init__(self, search, quote, delay=60):
         self.search = search
         self.quote = quote
         self.time = 0
+        self.delay = delay
 
-    def enough_time_passed():
-        return time.time() - self.time > 10
+    def enough_time_passed(self):
+        return time.time() - self.time > self.delay
 
+    def update_time(self):
+        self.time = time.time()
 
 class TestBot(irc.bot.SingleServerIRCBot):
-    quotes = {}
+    quotes = []
 
     def __init__(self, channel, nickname, server, port=6667):
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
@@ -57,8 +60,17 @@ class TestBot(irc.bot.SingleServerIRCBot):
         self.do_command(e, e.arguments[0])
 
     def on_pubmsg(self, c, e):
-        self.do_command(e, e.arguments[0])
-        return
+        user = e.source
+        channel = e.target
+        message = e.arguments[0] # always a list with one element
+
+        for q in self.quotes:
+            if q.search in message:
+                if q.enough_time_passed():
+                    c.privmsg(channel, q.quote)
+                    q.update_time()
+                else:
+                    print "Quote matched, but not enough time elapsed!"
 
     def on_dccmsg(self, c, e):
         c.privmsg("You said: " + e.arguments[0])
@@ -96,34 +108,36 @@ class TestBot(irc.bot.SingleServerIRCBot):
                 voiced = chobj.voiced()
                 voiced.sort()
                 c.notice(nick, "Voiced: " + ", ".join(voiced))
-        elif cmd == "dcc":
-            dcc = self.dcc_listen()
-            c.ctcp("DCC", nick, "CHAT chat %s %d" % (
-                ip_quad_to_numstr(dcc.localaddress),
-                dcc.localport))
+        # elif cmd == "dcc":
+        #     dcc = self.dcc_listen()
+        #     c.ctcp("DCC", nick, "CHAT chat %s %d" % (
+        #         ip_quad_to_numstr(dcc.localaddress),
+        #         dcc.localport))
         else:
             c.notice(nick, "Not understood: " + cmd)
 
 def main():
-    import sys
-    if len(sys.argv) != 4:
-        print "Usage: testbot <server[:port]> <channel> <nickname>"
-        sys.exit(1)
+    # TODO: read this stuff from a config file?
+    server = "irc.freenode.net"
+    port = 6667
 
-    s = sys.argv[1].split(":", 1)
-    server = s[0]
-    if len(s) == 2:
-        try:
-            port = int(s[1])
-        except ValueError:
-            print "Error: Erroneous port."
-            sys.exit(1)
-    else:
-        port = 6667
-    channel = sys.argv[2]
-    nickname = sys.argv[3]
+    channel = "#SpaceGoatsChat"
+    nickname = "SpaceGoatsBot"
 
     bot = TestBot(channel, nickname, server, port)
+
+    # TODO: load quotes from a file, or something
+    bot.quotes = [
+        Quote("test", "test passed"),
+        Quote("lunch", "http://www.youtube.com/watch?v=XU1wk2HWtwA"),
+        Quote("the mall", "http://www.youtube.com/watch?v=IY_bhVSGKEg"),
+
+        Quote("...", "...", delay=(60*5)),
+        Quote(">.>", "<.<", delay=(60*5)),
+        Quote("<.<", ">.>", delay=(60*5)),
+
+    ]
+
     bot.start()
 
 if __name__ == "__main__":
